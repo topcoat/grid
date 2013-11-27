83327f6548d7f23705cb14c01ba49c3517e88216
kristoferjoseph <kristoferjoseph@gmail.com>
2013-11-27 15:45:27 -0800

    Updating to publish gh-page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,25 @@
 module.exports = function(grunt) {
     grunt.initConfig({
 
+        autoprefixer: {
+            dist: {
+                options: {
+                    /*
+                     * Add target browsers here
+                     * https://github.com/ai/autoprefixer#browsers
+                     * browsers: ['android 4']
+                     */
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/'
+                }]
+            }
+
+        },
+
         cssmin: {
             minify: {
                 expand: true,
@@ -13,7 +32,8 @@ module.exports = function(grunt) {
 
     });
 
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['autoprefixer', 'cssmin']);
 };
